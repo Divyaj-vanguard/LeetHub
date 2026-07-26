f3ee46f0c7c1727caa958849d1de79389aad3b07
Adamya <djg491517@gmail.com>
2026-07-26 19:34:38 +0530

    Time: 0 ms (100%), Space: 10 MB (96.43%) - LeetHub

---
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.c
@@ -0,0 +1,36 @@
+int maximumProduct(int* nums, int numsSize) {
+    int i,max1=INT_MIN,max2=INT_MIN,max3=INT_MIN, min1=INT_MAX,min2=INT_MAX,first,second;
+    for (i=0;i<numsSize;i++){
+        if (nums[i]>max1){
+            max3=max2;
+            max2=max1;
+            max1=nums[i];
+        }
+        else if (nums[i]>max2){
+           max3=max2;
+           max2=nums[i];
+
+        }
+        else if (nums[i]>max3){
+            max3=nums[i];
+        }
+
+        if (nums[i]<min1){
+           min2=min1;
+           min1=nums[i];
+        }
+        else if (nums[i]<min2){
+            min2=nums[i];
+        }
+
+        }
+
+        first=min1*min2*max1;
+        second=max1*max2*max3;
+        if (first>second)
+        return first;
+        else
+        return second;
+}
+   
+    
